df6332b0f97fddbc185faced768bf68db3baa7e9
jab1012 <john.boucher@bioresource.nihr.ac.uk>
2020-10-16 15:13:05 +0100

    testing only

--- CRM/Nbrcustomtokens/NbrTokenValues.php
+++ CRM/Nbrcustomtokens/NbrTokenValues.php
@@ -9,33 +9,109 @@ use CRM_Nbrcustomtokens_ExtensionUtil as E;
 class CRM_Nbrcustomtokens_NbrTokenValues {
   /** Method to process the token values hook */
 
-  # job is blank, $context blank
+  public function tokenValues(&$values, $pids, $job, $tokens, $context) {
 
-  public function tokenValues(&$values, $cids, $job, $tokens, $context) {
+    Civi::log()->debug('');
+    Civi::log()->debug('-------- tokenvalues call --------');
+    Civi::log()->debug('PARAM $job : '.$job);
+    Civi::log()->debug('PARAM $context : '.$context);
+    Civi::log()->debug('PARAM $pids : '.implode('~',$pids));
+    Civi::log()->debug('PARAM $tokens - ');
+    foreach ($tokens as $key => $value) {
+      Civi::log()->debug('  token key : '.$key.'  token values : '.implode('~',$value));
+    }
+    Civi::log()->debug('PARAM &$values - ');
+    foreach ($values as $key => $value) {
+      if (is_array($value)) {
+        Civi::log()->debug('  $values key '.$key.' (Array) : ');
+        Civi::log()->debug('  $values value : '.implode('~',$value));
+      }
+      else {
+        Civi::log()->debug('  $values key : ' . $key . '  $values value : ' . $value);
+      }
+    }
 
-    if (isset($tokens['NBR_Stage_2'])) {
+    if (!empty($job)) {                                                                              # job id exists so bulk mail
+      $params = [1 => [$job, 'Integer']];
+      $query = "select r.contact_id as pid, m.study_id as study_id, MJ.mailing_id as mailing_id
+                from civicrm_mailing_job mj, civicrm_nbr_mailing m, civicrm_mailing_recipients r
+                where m.mailing_id = mj.mailing_id and m.mailing_id = r.mailing_id
+                and mj.id = %1";
+      $dao = CRM_Core_DAO::executeQuery($query, $params);
+      while ($dao->fetch()) {                                                                       # for each pid
+        $study_id = $dao->study_id;                                                                 #  get study_id
+        $pid = $dao-> pid;
+        $caseId = CRM_Nihrbackbone_NbrVolunteerCase::getActiveParticipationCaseId($study_id, $pid); #  and case_id
+        if (isset($tokens['NBR_Stage_2'])) {                                                        #  set stage2 tokens for pid
+          $this->setStage2TokenValues($values, $pid, $caseId);
+        }
+        if (isset($tokens['NBR_Contact'])) {
+          $this->setNbrContactTokenValues($values, $pid);                                           # set contact tokens for pid
+        }
+      }
+    }
+    else {                                                                                          # non-bulk email
+      Civi::log()->debug('not a bulk mailing');
 
-      if (!is_array($cids)){$cids = [$cids];}
+      if (!is_array($pids)){$pids = [$pids];}
 
-      foreach ($cids as $cid) {
-
-        $caseId = CRM_Utils_Request::retrieveValue("caseid", "Integer"); # get case id from url or $values array
-
-        if (!$caseId) {
-          if (isset($values[$cid]['case_id'])) {
-            $caseId = $values[$cid]['case_id'];
+      foreach ($pids as $pid) {                                                                     # for each pid
+        $caseId = CRM_Utils_Request::retrieveValue("caseid", "Integer");                            #  get case id from url ..
+        if (!$caseId) {                                                                             #  or $values array ..
+          if (isset($values[$pid]['case_id'])) {
+            $caseId = $values[$pid]['case_id'];
           }
-          elseif (isset($values[$cid]['case.id'])) {
-            $caseId = $values[$cid]['case.id'];
+          elseif (isset($values[$pid]['case.id'])) {
+            $caseId = $values[$pid]['case.id'];
           }
         }
+        Civi::log()->debug('$caseId : '.$caseId);
 
         if ($caseId) {
+          if (isset($tokens['NBR_Stage_2'])) {                                                      #  set stage2 tokens for pid
+            $this->setStage2TokenValues($values, $pid, $caseId);
+          }
+          if (isset($tokens['NBR_Contact'])) {                                                      # set contact tokens for pid
+            $this->setNbrContactTokenValues($values, $pid);
+          }
+        } # if case id
 
-          $params = [1 => [$cid, 'Integer'], 2 => [$caseId, 'Integer'],];
-          $query = 'select sd.nsd_study_number as study_number, sd.nsd_study_long_name as study_long_name, camp.name as study_short_name, rcont.display_name as researcher,
+      } # for each
+
+    } # if empty job
+
+    Civi::log()->debug('ON EXIT &$values Array : ');
+    foreach ($values as $key => $value) {
+      if (is_array($value)) {
+        Civi::log()->debug('$key : '.$key.'  - Array : ');
+        Civi::log()->debug('$vals : '.implode('~',$value));
+      }
+      else {
+        Civi::log()->debug('$key : ' . $key . '  $value : ' . $value);
+      }
+    }
+    Civi::log()->debug('----------------------------');
+
+  } # funct
+
+  public function setNbrContactTokenValues(&$values, $pid) {
+    Civi::log()->debug('setNbrContactTokenValues called for $pid : '.$pid);
+    $params = [1 => [$pid, 'Integer']];
+    $query = "select nva_participant_id, nva_bioresource_id from civicrm_value_nihr_volunteer_ids where entity_id = %1";
+    $dao = CRM_Core_DAO::executeQuery($query, $params);
+    if ($dao->fetch()) {
+      $values[$pid]['NBR_Contact.participant_id'] = $dao->nva_participant_id;
+      $values[$pid]['NBR_Contact.bioresource_id'] = $dao->nva_bioresource_id;
+    }
+  }
+
+  public function setStage2TokenValues(&$values, $pid, $caseId){
+    Civi::log()->debug('setStage2TokenValues called for $pid : '.$pid.'  $caseId : '.$caseId);
+    $params = [1 => [$pid, 'Integer'], 2 => [$caseId, 'Integer'],];
+    $query = 'select sd.nsd_study_number as study_number, sd.nsd_study_long_name as study_long_name, camp.name as study_short_name, rcont.display_name as researcher,
             radd.street_address as r_addr0, radd.supplemental_address_1 as r_addr1, radd.supplemental_address_2 as r_addr2, radd.supplemental_address_3 as r_addr3,
-            radd.postal_code as r_pcode, email.email as r_email, pcont.display_name as investigator, sd.nsd_scientific_info as study_text,  sd.nsd_ethics_number as study_ethics
+            radd.postal_code as r_pcode, email.email as r_email, pcont.display_name as investigator, sd.nsd_scientific_info as study_text,  sd.nsd_ethics_number as study_ethics,
+            sd.nsd_lay_summary as study_summary
             from civicrm_case_contact cc
             join civicrm_case cas on cc.case_id = cas.id
             left join civicrm_value_nbr_participation_data pd on cc.case_id = pd.entity_id
@@ -46,41 +122,24 @@ class CRM_Nbrcustomtokens_NbrTokenValues {
             left join civicrm_address radd on sd.nsd_researcher = radd.contact_id
             left join civicrm_email email on radd.contact_id = email.contact_id
             where cc.contact_id = %1 and cc.case_id = %2 and cas.is_deleted = 0 limit 1';
-
-          $dao = CRM_Core_DAO::executeQuery($query, $params);
-          if ($dao->fetch()) {
-            $values[$cid]['NBR_Stage_2.study_number'] = $dao->study_number;
-            $values[$cid]['NBR_Stage_2.study_short_name'] = $dao->study_short_name;
-            $values[$cid]['NBR_Stage_2.study_long_name'] = $dao->study_long_name;
-            $values[$cid]['NBR_Stage_2.investigator_name'] = $dao->investigator;
-            $values[$cid]['NBR_Stage_2.researcher_name'] = $dao->researcher;
-            $values[$cid]['NBR_Stage_2.researcher_address0'] = $dao->r_addr0;
-            $values[$cid]['NBR_Stage_2.researcher_address1'] = $dao->r_addr1;
-            $values[$cid]['NBR_Stage_2.researcher_address2'] = $dao->r_addr2;
-            $values[$cid]['NBR_Stage_2.researcher_address3'] = $dao->r_addr3;
-            $values[$cid]['NBR_Stage_2.researcher_pcode'] = $dao->r_pcode;
-            $values[$cid]['NBR_Stage_2.researcher_email'] = $dao->r_email;
-            $values[$cid]['NBR_Stage_2.study_text'] = $dao->study_text;
-            $values[$cid]['NBR_Stage_2.study_ethics_number'] = $dao->study_ethics;
-          }
-
-        }
-
-      }
-
+    $dao = CRM_Core_DAO::executeQuery($query, $params);
+    if ($dao->fetch()) {
+      $values[$pid]['NBR_Stage_2.study_number'] = $dao->study_number;
+      $values[$pid]['NBR_Stage_2.study_short_name'] = $dao->study_short_name;
+      $values[$pid]['NBR_Stage_2.study_long_name'] = $dao->study_long_name;
+      $values[$pid]['NBR_Stage_2.investigator_name'] = $dao->investigator;
+      $values[$pid]['NBR_Stage_2.researcher_name'] = $dao->researcher;
+      $values[$pid]['NBR_Stage_2.researcher_address0'] = $dao->r_addr0;
+      $values[$pid]['NBR_Stage_2.researcher_address1'] = $dao->r_addr1;
+      $values[$pid]['NBR_Stage_2.researcher_address2'] = $dao->r_addr2;
+      $values[$pid]['NBR_Stage_2.researcher_address3'] = $dao->r_addr3;
+      $values[$pid]['NBR_Stage_2.researcher_pcode'] = $dao->r_pcode;
+      $values[$pid]['NBR_Stage_2.researcher_email'] = $dao->r_email;
+      $values[$pid]['NBR_Stage_2.study_text'] = $dao->study_text;
+      $values[$pid]['NBR_Stage_2.study_ethics_number'] = $dao->study_ethics;
+      $values[$pid]['NBR_Stage_2.study_lay_summary'] = $dao->study_summary;
+      #Civi::log()->debug('setTokenValues value for study_number : '.strval($dao->study_number));
     }
-
-    if (isset($tokens['NBR_Contact'])) {
-      $params = [1 => [$cid, 'Integer']];
-      $query = "select nva_participant_id, nva_bioresource_id from civicrm_value_nihr_volunteer_ids where entity_id = %1";
-      $dao = CRM_Core_DAO::executeQuery($query, $params);
-      if ($dao->fetch()) {
-        $values[$cid]['NBR_Contact.participant_id'] = $dao->nva_participant_id;
-        $values[$cid]['NBR_Contact.bioresource_id'] = $dao->nva_bioresource_id;
-      }
-
-    }
-
   }
 
-}
+} # class
